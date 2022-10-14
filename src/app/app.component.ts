1a4995ee9c60b05c58bca428b3a1db2c0db5f651
babarich1995 <babarich1995@yahoo.com>
2022-10-14 13:02:59 +0300

    angular task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,41 @@ import TableJson from './data.json'
 export class AppComponent implements OnInit{
   title = 'angulartask';
   tableDemsions:any = TableJson;
-
+  datarowNew:any;
+  result:any
+  period = this.tableDemsions.metaData.dimensions.pe
+   final:any
  
   constructor(){
-    console.log(this.tableDemsions)
+   let  period = this.tableDemsions.metaData.dimensions.pe
+   const  data =this.tableDemsions.metaData.dimensions.dx
+   const   organizationunit =this.tableDemsions.metaData.dimensions.ou
+   let dataRow =this.tableDemsions.rows
+   
+const datarowNew = [period, data, organizationunit, dataRow].filter(arr => arr.period== arr.dataRow);
+const result = [...new Set(datarowNew)];
+let final = Array.of<any>();
+for (const key of result) {
+    final = final.concat(key);
+}
+   
+  
   }
 
   ngOnInit(){
   this.tableDemsions = Array.of(this.tableDemsions)
 
+  
   }
-
+  public firstData(str: any) {
+    const Country = this.tableDemsions.metaData.dimensions.pe
+    console.log(Country)
+    return (
+      this.tableDemsions.filter((s) => s.rows == str.Country).indexOf(str) == 0
+    );
+  }
+  objectKeys(obj) {
+    return Object.keys(obj);
+}
 
 }
